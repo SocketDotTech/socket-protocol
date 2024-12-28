821894f96a4a3569ae89920fd6511866c2e04820
Rookmate <14072042+rookmate@users.noreply.github.com>
2024-12-28 10:54:21 +0000

    Rename Counter example

--- contracts/apps/counter/Counter.sol
+++ contracts/apps/counter/Counter.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 
-import "../../utils/Ownable.sol";
+import "socket-protocol/contracts/utils/Ownable.sol";
 
 contract Counter is Ownable(msg.sender) {
     address public socket;
@@ -16,7 +16,11 @@ contract Counter is Ownable(msg.sender) {
         socket = _socket;
     }
 
-    function setCounter(uint256 _counter) external onlySocket {
-        counter = _counter;
+    function getSocket() external view returns (address) {
+        return socket;
+    }
+
+    function increase() external onlySocket {
+        counter++;
     }
 }
--- contracts/apps/counter/CounterDeployer.sol
+++ contracts/apps/counter/CounterDeployer.sol
@@ -1,17 +1,13 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 
-import "../Counter.sol";
-import "../../../base/AppDeployerBase.sol";
-import "../../../utils/Ownable.sol";
+import "./Counter.sol";
+import "socket-protocol/contracts/base/AppDeployerBase.sol";
 
-contract CounterDeployer is AppDeployerBase, Ownable {
+contract CounterDeployer is AppDeployerBase {
     bytes32 public counter = _createContractId("counter");
 
-    constructor(
-        address addressResolver_,
-        FeesData memory feesData_
-    ) AppDeployerBase(addressResolver_) Ownable(msg.sender) {
+    constructor(address addressResolver_, FeesData memory feesData_) AppDeployerBase(addressResolver_) {
         creationCodeWithArgs[counter] = type(Counter).creationCode;
         _setFeesData(feesData_);
     }
@@ -24,5 +20,9 @@ contract CounterDeployer is AppDeployerBase, Ownable {
         address socket = getSocketAddress(chainSlug);
         address counterForwarder = forwarderAddresses[counter][chainSlug];
         Counter(counterForwarder).setSocket(socket);
+    }
+
+    function setFees(FeesData memory feesData_) public {
+        feesData = feesData_;
     }
 }
