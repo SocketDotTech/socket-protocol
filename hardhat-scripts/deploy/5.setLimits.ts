df78d8c49886c0b01ebeaa29be1c851e0f87dc3e
Ameesha Agrawal <ameeshaagrawal12@gmail.com>
2025-01-08 00:56:15 +0530

    feat: limit scripts

---
+++ hardhat-scripts/deploy/5.setLimits.ts
@@ -0,0 +1,130 @@
+import { DeploymentAddresses } from "@socket.tech/dl-core";
+import { Contract, ethers, providers } from "ethers";
+import WatcherABI from "../../out/WatcherPrecompile.sol/WatcherPrecompile.json";
+import { OffChainVMCoreContracts } from "./config";
+import dev_addresses from "../../deployments/dev_addresses.json";
+import { OFF_CHAIN_VM_CHAIN_ID } from "../constants/constants";
+
+const MAX_LIMIT = "10000000000000000000000";
+const providerInstance = new providers.StaticJsonRpcProvider(
+  process.env.OFF_CHAIN_VM_RPC as string
+);
+const signer = new ethers.Wallet(
+  process.env.WATCHER_PRIVATE_KEY as string,
+  providerInstance
+);
+type LimitParam = {
+  limitType: string;
+  appGateway: string;
+  maxLimit: string;
+  ratePerSecond: string;
+};
+
+// Set limits on the Watcher VM
+export const updateContractLimits = async () => {
+  try {
+    console.log("Setting limits on OffChainVM");
+
+    const addresses = dev_addresses as unknown as DeploymentAddresses;
+    const limitParamsToSet: LimitParam[] = [
+      {
+        limitType: ethers.utils.keccak256(ethers.utils.toUtf8Bytes("FINALIZE")),
+        appGateway:
+          addresses[OFF_CHAIN_VM_CHAIN_ID]?.[
+            OffChainVMCoreContracts.DeliveryHelper
+          ],
+        maxLimit: MAX_LIMIT,
+        ratePerSecond: MAX_LIMIT,
+      },
+      {
+        limitType: ethers.utils.keccak256(ethers.utils.toUtf8Bytes("QUERY")),
+        appGateway:
+          addresses[OFF_CHAIN_VM_CHAIN_ID]?.[
+            OffChainVMCoreContracts.DeliveryHelper
+          ],
+        maxLimit: MAX_LIMIT,
+        ratePerSecond: MAX_LIMIT,
+      },
+      {
+        limitType: ethers.utils.keccak256(ethers.utils.toUtf8Bytes("SCHEDULE")),
+        appGateway:
+          addresses[OFF_CHAIN_VM_CHAIN_ID]?.[
+            OffChainVMCoreContracts.AuctionManager
+          ],
+        maxLimit: MAX_LIMIT,
+        ratePerSecond: MAX_LIMIT,
+      },
+      {
+        limitType: ethers.utils.keccak256(ethers.utils.toUtf8Bytes("FINALIZE")),
+        appGateway:
+          addresses[OFF_CHAIN_VM_CHAIN_ID]?.[
+            OffChainVMCoreContracts.FeesManager
+          ],
+        maxLimit: MAX_LIMIT,
+        ratePerSecond: MAX_LIMIT,
+      },
+    ];
+
+    const limitParams: LimitParam[] = [];
+
+    // Set up Watcher contract
+    const watcherVMaddr = addresses[OFF_CHAIN_VM_CHAIN_ID]!;
+    const watcher = new Contract(
+      watcherVMaddr[OffChainVMCoreContracts.WatcherPrecompile],
+      WatcherABI.abi,
+      signer
+    );
+
+    // Collect configs for each chain and plug
+    for (const limitParam of limitParamsToSet) {
+      const appGateway = limitParam.appGateway;
+
+      if (await isLimitSet(watcher, limitParam.limitType, appGateway)) {
+        console.log(`Limit already set on ${appGateway}`);
+        continue;
+      }
+      limitParams.push(limitParam);
+    }
+
+    // Update configs if any changes needed
+    if (limitParams.length > 0) {
+      console.log({ limitParams });
+      const tx = await watcher.updateLimitParams(limitParams);
+      console.log(`Updating OffChainVM limits tx hash: ${tx.hash}`);
+      await tx.wait();
+    }
+  } catch (error) {
+    console.log("Error while sending transaction to set limits", error);
+  }
+};
+
+async function isLimitSet(
+  watcher: Contract,
+  limitType: string,
+  appGateway: string
+): Promise<boolean> {
+  try {
+    const currentLimit = await watcher.getCurrentLimit(limitType, appGateway);
+    // If maxLimit is 0, limit is not set yet
+    return currentLimit.maxLimit.eq(MAX_LIMIT);
+  } catch (error) {
+    console.log(`Error checking limit for ${appGateway}:`, error);
+    return false;
+  }
+}
+
+// Main function to set limits
+export const main = async () => {
+  try {
+    await updateContractLimits();
+  } catch (error) {
+    console.log("Error while sending transaction to set limits", error);
+  }
+};
+
+main()
+  .then(() => process.exit(0))
+  .catch((error: Error) => {
+    console.error(error);
+    process.exit(1);
+  });
