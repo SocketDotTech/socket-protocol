6c6f7e285c5019ffe6ab31e80e3db9c8ae1c92af
Ameesha Agrawal <ameeshaagrawal12@gmail.com>
2025-01-28 16:15:00 +0530

    feat: default limits

--- contracts/watcherPrecompile/WatcherPrecompileLimits.sol
+++ contracts/watcherPrecompile/WatcherPrecompileLimits.sol
@@ -4,10 +4,25 @@ pragma solidity ^0.8.21;
 import {Gauge} from "../utils/Gauge.sol";
 import {LimitParams, UpdateLimitParams} from "../common/Structs.sol";
 import {AddressResolverUtil} from "../utils/AddressResolverUtil.sol";
+import "../interfaces/IWatcherPrecompile.sol";
+import {OwnableTwoStep} from "../utils/OwnableTwoStep.sol";
+import {QUERY, FINALIZE, SCHEDULE} from "../common/Constants.sol";
 
-abstract contract WatcherPrecompileLimits is Gauge, AddressResolverUtil {
-    // appGateway => receivingLimitParams
+abstract contract WatcherPrecompileLimits is
+    Gauge,
+    AddressResolverUtil,
+    OwnableTwoStep,
+    IWatcherPrecompile
+{
+    uint256 public maxLimit;
+    uint256 public ratePerSecond;
+    uint256 public LIMIT_DECIMALS;
+
+    // appGateway => limitType => receivingLimitParams
     mapping(address => mapping(bytes32 => LimitParams)) internal _limitParams;
+
+    // Mapping to track active app gateways
+    mapping(address => bool) private _activeAppGateways;
 
     ////////////////////////////////////////////////////////
     ////////////////////// EVENTS //////////////////////////
@@ -16,6 +31,7 @@ abstract contract WatcherPrecompileLimits is Gauge, AddressResolverUtil {
     // Emitted when limit parameters are updated
     event LimitParamsUpdated(UpdateLimitParams[] updates);
     error ActionNotSupported(address appGateway_, bytes32 limitType_);
+    error NotDeliveryHelper();
 
     function getCurrentLimit(
         bytes32 limitType_,
@@ -29,6 +45,24 @@ abstract contract WatcherPrecompileLimits is Gauge, AddressResolverUtil {
         bytes32 limitType_
     ) external view returns (LimitParams memory) {
         return _limitParams[appGateway_][limitType_];
+    }
+
+    /**
+     * @notice Checks, updates, and reverts based on the limit.
+     * @param appGateway_ The app gateway address to check limits for
+     * @param limitType_ The type of limit to check
+     */
+    function checkAndUpdateLimit(
+        address appGateway_,
+        bytes32 limitType_,
+        uint256 consumeLimit_
+    ) external {
+        if (msg.sender != addressResolver__.deliveryHelper()) revert NotDeliveryHelper();
+        _consumeLimit(appGateway_, limitType_, consumeLimit_);
+    }
+
+    function updateLimitParams(UpdateLimitParams[] calldata updates_) external onlyOwner {
+        _updateLimitParams(updates_);
     }
 
     /**
@@ -55,23 +89,47 @@ abstract contract WatcherPrecompileLimits is Gauge, AddressResolverUtil {
      */
     function _consumeLimit(
         address appGateway_,
-        bytes32 limitType_
+        bytes32 limitType_,
+        uint256 consumeLimit_
     ) internal returns (address appGateway) {
+        if (msg.sender != addressResolver__.deliveryHelper()) return;
         appGateway = _getAppGateway(appGateway_);
-        if (_limitParams[appGateway][limitType_].maxLimit == 0)
-            revert ActionNotSupported(appGateway, limitType_);
+        LimitParams storage limitParams = _limitParams[appGateway][limitType_];
 
-        // Reverts on limit hit
-        _consumeFullLimit(1, _limitParams[appGateway][limitType_]);
+        // Initialize limit if not active
+        if (!_activeAppGateways[appGateway]) {
+            LimitParams memory limitParam = LimitParams({
+                maxLimit: maxLimit,
+                ratePerSecond: ratePerSecond,
+                lastUpdateTimestamp: block.timestamp,
+                lastUpdateLimit: maxLimit
+            });
+
+            _limitParams[appGateway][QUERY] = limitParam;
+            _limitParams[appGateway][FINALIZE] = limitParam;
+            _limitParams[appGateway][SCHEDULE] = limitParam;
+
+            _activeAppGateways[appGateway] = true;
+        }
+
+        // Update the limit
+        _consumeFullLimit(consumeLimit_, limitParams);
     }
 
     function _getAppGateway(address appGateway_) internal view returns (address appGateway) {
-        address resolverAddress = msg.sender == addressResolver__.deliveryHelper() ||
-            msg.sender == addressResolver__.feesManager()
+        address resolverAddress = msg.sender == addressResolver__.deliveryHelper()
             ? appGateway_
             : msg.sender;
 
         appGateway = _getCoreAppGateway(resolverAddress);
+    }
+
+    function setMaxLimit(uint256 maxLimit_) external onlyOwner {
+        maxLimit = maxLimit_;
+    }
+
+    function setRatePerSecond(uint256 ratePerSecond_) external onlyOwner {
+        ratePerSecond = ratePerSecond_;
     }
 
     uint256[49] __gap;
