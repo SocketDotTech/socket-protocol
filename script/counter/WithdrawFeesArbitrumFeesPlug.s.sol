1a6c9bcf0ba34d4f0c39175b0fb6274ee67cd45e
Rookmate <14072042+rookmate@users.noreply.github.com>
2025-02-21 13:09:32 +0000

    [WIP] Take into account ArbSep gas price WithdrawFeesArbitrumFeesPlug

--- script/counter/WithdrawFeesArbitrumFeesPlug.s.sol
+++ script/counter/WithdrawFeesArbitrumFeesPlug.s.sol
@@ -9,6 +9,7 @@ import {CounterAppGateway} from "../../contracts/apps/counter/CounterAppGateway.
 
 contract WithdrawFees is Script {
     function run() external {
+        // EVMX Check available fees
         vm.createSelectFork(vm.envString("EVMX_RPC"));
         FeesManager feesManager = FeesManager(payable(vm.envAddress("FEES_MANAGER")));
         address appGatewayAddress = vm.envAddress("APP_GATEWAY");
@@ -22,20 +23,39 @@ contract WithdrawFees is Script {
         console.log("Available fees:", availableFees);
 
         if (availableFees > 0) {
+            // Switch to Arbitrum Sepolia to get gas price
             vm.createSelectFork(vm.envString("ARBITRUM_SEPOLIA_RPC"));
-
             uint256 privateKey = vm.envUint("PRIVATE_KEY");
             address sender = vm.addr(privateKey);
-            console.log("Sender address:", sender);
-            console.log("Sender balance", sender.balance);
 
-            vm.createSelectFork(vm.envString("EVMX_RPC"));
-            vm.startBroadcast(privateKey);
+            // Gas price from Arbitrum
+            uint256 arbitrumGasPrice = block.basefee + 0.1 gwei;
+            uint256 gasLimit = 300000; // Estimate
+            uint256 estimatedGasCost = gasLimit * arbitrumGasPrice;
 
-            appGateway.withdrawFeeTokens(421614, ETH_ADDRESS, availableFees, sender);
-            console.log("Withdrew:", availableFees);
-            vm.createSelectFork(vm.envString("ARBITRUM_SEPOLIA_RPC"));
-            console.log("Sender balance", sender.balance);
+            console.log("Arbitrum gas price (wei):", arbitrumGasPrice);
+            console.log("Gas limit:", gasLimit);
+            console.log("Estimated gas cost:", estimatedGasCost);
+
+            // Calculate amount to withdraw
+            uint256 amountToWithdraw = availableFees > estimatedGasCost
+                ? availableFees - estimatedGasCost
+                : 0;
+
+            if (amountToWithdraw > 0) {
+                // Switch back to EVMX to perform withdrawal
+                vm.createSelectFork(vm.envString("EVMX_RPC"));
+                vm.startBroadcast(privateKey);
+
+                console.log("Withdrawing amount:", amountToWithdraw);
+                appGateway.withdrawFeeTokens(421614, ETH_ADDRESS, amountToWithdraw, sender);
+
+                // Switch back to Arbitrum Sepolia to check final balance
+                vm.createSelectFork(vm.envString("ARBITRUM_SEPOLIA_RPC"));
+                console.log("Final sender balance:", sender.balance);
+            } else {
+                console.log("Available fees less than estimated gas cost");
+            }
         }
     }
 }
