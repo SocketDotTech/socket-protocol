a1cc45f896b0d8f294b8fcb6c4cc72b4c0b5875a
Rookmate <14072042+rookmate@users.noreply.github.com>
2025-02-21 10:44:14 +0000

    [WIP] Update WithdrawFees

--- script/counter/WithdrawFeesArbitrumFeesPlug.s.sol
+++ script/counter/WithdrawFeesArbitrumFeesPlug.s.sol
@@ -3,30 +3,37 @@ pragma solidity ^0.8.0;
 
 import {Script} from "forge-std/Script.sol";
 import {console} from "forge-std/console.sol";
-import {FeesPlug} from "../../contracts/protocol/payload-delivery/FeesPlug.sol";
+import {FeesManager} from "../../contracts/protocol/payload-delivery/app-gateway/FeesManager.sol";
 import {ETH_ADDRESS} from "../../contracts/protocol/utils/common/Constants.sol";
 import {CounterAppGateway} from "../../contracts/apps/counter/CounterAppGateway.sol";
 
 contract WithdrawFees is Script {
     function run() external {
-        vm.createSelectFork(vm.envString("ARBITRUM_SEPOLIA_RPC"));
-
-        uint256 privateKey = vm.envUint("PRIVATE_KEY");
-        vm.startBroadcast(privateKey);
-        FeesPlug feesPlug = FeesPlug(payable(vm.envAddress("ARBITRUM_FEES_PLUG")));
+        vm.createSelectFork(vm.envString("EVMX_RPC"));
+        FeesManager feesManager = FeesManager(payable(vm.envAddress("FEES_MANAGER")));
         address appGatewayAddress = vm.envAddress("APP_GATEWAY");
         CounterAppGateway appGateway = CounterAppGateway(appGatewayAddress);
 
-        address sender = vm.addr(privateKey);
-        console.log("Sender address:", sender);
-        uint256 balance = sender.balance;
-        console.log("Sender balance:", balance);
+        uint256 availableFees = feesManager.getAvailableFees(
+            421614,
+            appGatewayAddress,
+            ETH_ADDRESS
+        );
+        console.log("Available fees:", availableFees);
 
-        uint256 appBalance = feesPlug.balanceOf(appGatewayAddress);
-        console.log("AppBalance:", appBalance);
-        if (appBalance > 0) {
-            appGateway.withdrawFeeTokens(421614, ETH_ADDRESS, appBalance, sender);
-            console.log("Withdrew:", appBalance);
+        if (availableFees > 0) {
+            vm.createSelectFork(vm.envString("ARBITRUM_SEPOLIA_RPC"));
+
+            uint256 privateKey = vm.envUint("PRIVATE_KEY");
+            address sender = vm.addr(privateKey);
+            console.log("Sender address:", sender);
+            console.log("Sender balance", sender.balance);
+
+            vm.startBroadcast(privateKey);
+
+            appGateway.withdrawFeeTokens(421614, ETH_ADDRESS, availableFees, sender);
+            console.log("Withdrew:", availableFees);
+            console.log("Sender balance", sender.balance);
         }
     }
 }
