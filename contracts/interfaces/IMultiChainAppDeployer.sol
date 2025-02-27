1fb422c54b74af6f22a5f6ca9eeb65b6cb0e9918
Ameesha Agrawal <ameeshaagrawal12@gmail.com>
2025-02-27 14:52:06 +0400

    fix: remove interface

--- contracts/interfaces/IMultiChainAppDeployer.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.21;
-
-/// @title IMultiChainAppDeployer
-/// @notice Interface for the multi-chain app deployer
-interface IMultiChainAppDeployer {
-    /// @notice deploy contracts to multiple chains
-    function deployMultiChainContracts(uint32[] memory chainSlugs_) external;
-}
