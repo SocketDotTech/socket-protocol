aed17ca23e9bdfc4097987a5f8f4d09c38b3a66e
arthcp <arth.c.patel@gmail.com>
2025-03-24 15:19:05 +0400

    feat: transmitter address for role script

--- hardhat-scripts/config/config.ts
+++ hardhat-scripts/config/config.ts
@@ -37,6 +37,7 @@ export const EVM_CHAIN_ID_MAP: Record<DeploymentMode, number> = {
 };
 export const auctionEndDelaySeconds = 0;
 export const watcher = "0xb62505feacC486e809392c65614Ce4d7b051923b";
+export const transmitter = "todo";
 export const MAX_FEES = ethers.utils.parseEther("0.001");
 export const EVMX_CHAIN_ID = EVM_CHAIN_ID_MAP[mode];
 export const DEFAULT_MAX_LIMIT = 100;
